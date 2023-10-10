bb209691ce4e5581ce8a5e595b762dee66e3fbf0
Simangeletutubala <simangeletutubala@gmail.com>
2023-10-10 15:12:25 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,7 +11,7 @@ void print_alphabet_x10(void)
 char ch;
 int i;
 i = 0;
-while ( i < 10)
+while (i < 10)
 {
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
@@ -20,7 +20,6 @@ _putchar(ch);
 _putchar('\n');
 i++;
 }
-
 
 }
 
