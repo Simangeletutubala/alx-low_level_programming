3cd6caadf4cfb20984fa7d10f30e35f4dff488da
Simangeletutubala <simangeletutubala@gmail.com>
2023-10-10 13:24:22 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,13 @@
  * print_alphabet -Make the alphabet
  *
  * Return:void
+ *
  */
+
 void print_alphabet(void)
 {
-	char ch; 
-	for (ch = 'a'; ch <= 'z'; ch++)
-	_putchar(ch);
-	_putchar('\n');
+char ch; 
+for (ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
 }
